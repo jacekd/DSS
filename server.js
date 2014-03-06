1b8b1588f0a4fd54c7ade3551f81449defeed269
Jacek Dominiak <jacek@effortable.com>
2014-03-06 16:02:11 +0100

    orientdb test query working

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express'),
     config = require('./config'),
     http = require('http'),
+    Orientdb = require('node-orientdb');
+    Db = Orientdb.GraphDb;
     app = exports.app =  express();
 
 // environment setup
@@ -11,6 +13,44 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
+
+// Database config
+var db = exports.db = new Db({
+  //Server
+  server_host: config.database.host,
+  server_port: config.database.port,
+  server_username: config.database.serverUsername,
+  server_password: config.database.serverPassword,
+
+  //Database
+  database_name: config.database.databaseName,
+  database_username: config.database.databaseUser,
+  database_password: config.database.databasePassword
+});
+
+db.open(function (err) {
+  if (err) throw err;
+})
+    .then(function(results) {
+      //Details
+      console.log("Database '" + db.databaseName + "' has " + db.clusters.length + " clusters");
+
+      //SQL Statement
+      var sql  = 'SELECT FROM OUser';
+      var opts = {};
+
+      //Queries
+      db.query(sql, opts)
+          .then(function(results) {
+            console.log(results);
+          })
+          .error(function(error) {
+            console.log(error);
+          });
+    })
+    .error(function(error) {
+      console.log(error);
+    });
 
 // start server
 http.createServer(app).listen(app.get('port'), function () {
