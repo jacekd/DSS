af3f3f70993976a6220ae63466f68700dbdef7fb
Jacek Dominiak <jacek@effortable.com>
2014-03-11 15:22:12 +0100

    change of indentation

--- routes/dss.js
+++ routes/dss.js
@@ -2,28 +2,28 @@
 var _ = require('underscore');
 
 var DSSRoute = function (app, db) {
-  app.get('/resources', function (req, res) {
+    app.get('/resources', function (req, res) {
 
-    db.query('SELECT FROM Resources')
-        .then(function (results) {
-          return res.json(200, results);
-        })
-        .error(function (error) {
-          return res.json(500, error);
-        });
-  });
+        db.query('SELECT FROM Resources')
+            .then(function (results) {
+                return res.json(200, results);
+            })
+            .error(function (error) {
+                return res.json(500, error);
+            });
+    });
 
-  app.get('/query', function (req, res) {
+    app.get('/query', function (req, res) {
 
-    db.query(req.query.query.toString())
-        .then(function (results) {
-          if (_.isEmpty(results)) return res.json(404, '');
-          return res.json(200, results);
-        })
-        .error(function (error) {
-          return res.json(404, error);
-        });
-  });
+        db.query(req.query.query.toString())
+            .then(function (results) {
+                if (_.isEmpty(results)) return res.json(404, '');
+                return res.json(200, results);
+            })
+            .error(function (error) {
+                return res.json(404, error);
+            });
+    });
 };
 
 module.exports = DSSRoute;
--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express'),
     config = require('./config'),
     http = require('http'),
     Orientdb = require('node-orientdb');
-    Db = Orientdb.GraphDb;
-    app = exports.app =  express();
+Db = Orientdb.GraphDb;
+app = exports.app =  express();
 
 // environment setup
 app.set('port', config.application.port || 3000);
@@ -16,25 +16,25 @@ app.use(app.router);
 
 // Database config
 var db = new Db({
-  //Server
-  server_host: config.database.host,
-  server_port: config.database.port,
-  server_username: config.database.serverUsername,
-  server_password: config.database.serverPassword,
+    //Server
+    server_host: config.database.host,
+    server_port: config.database.port,
+    server_username: config.database.serverUsername,
+    server_password: config.database.serverPassword,
 
-  //Database
-  database_name: config.database.databaseName,
-  database_username: config.database.databaseUser,
-  database_password: config.database.databasePassword
+    //Database
+    database_name: config.database.databaseName,
+    database_username: config.database.databaseUser,
+    database_password: config.database.databasePassword
 });
 
 exports.db = db.open()
     .then(function () {
-      console.log(app.get('name') + ' database: connected OK');
+        console.log(app.get('name') + ' database: connected OK');
     })
     .error(function (error) {
-      db.close();
-      console.log(error);
+        db.close();
+        console.log(error);
     });
 
 
@@ -43,5 +43,5 @@ require('./routes/dss')(app, db);
 
 // start server
 http.createServer(app).listen(app.get('port'), function () {
-  console.log(app.get('name') + ' server: started [port: ' + app.get('port') + ']');
+    console.log(app.get('name') + ' server: started [port: ' + app.get('port') + ']');
 });
