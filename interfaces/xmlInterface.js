656195f76fc3cfbffafa08cb9f787785d401c1ba
Jacek Dominiak <jacek@effortable.com>
2014-03-13 15:13:03 +0100

    base xml parser... really basic

--- interfaces/xmlInterface.js
+++ interfaces/xmlInterface.js
@@ -1,1 +1,8 @@
-var _ = require('underscore');
+var parseString = require('xml2js').parseString;
+
+exports.parseXML = function (xmlString) {
+    parseString(xmlString, {trim: true}, function (error, result) {
+       if (error) return error;
+       return result;
+    });
+};
--- routes/wsdl.js
+++ routes/wsdl.js
@@ -1,2 +1,16 @@
 var _ = require('underscore'),
     xmlInterface = require('./../interfaces/xmlInterface');
+
+var WSDLroute = function (app, db) {
+
+    app.post('/wsdl', function (req, res) {
+
+        var XMLObject = xmlInterface.parseXML(req.body.xml);
+
+        _.each(XMLObject, function (value, key) {
+
+        });
+    });
+};
+
+module.exports = WSDLroute;
