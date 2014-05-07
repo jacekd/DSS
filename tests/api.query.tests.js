aa0de81a856b20f90cd39c2ec570abb5b5c982d6
Jacek Dominiak <jacek@effortable.com>
2014-05-07 15:54:10 +0200

    delete dummy test

--- tests/api.query.tests.js
+++ tests/api.query.tests.js
@@ -14,5 +14,16 @@ describe('API tests', function () {
                    done();
                });
        });
+
+       it('should be able to run the query with "with"', function (done) {
+           request(app)
+               .get('/query/assets')
+               .expect(200)
+               .end(function (err, res) {
+                  if (err) throw err;
+                  res.body.should.not.be.empty;
+                  done();
+               });
+       });
    });
 });
--- tests/routes.tests.js
+++
@@ -1,5 +0,0 @@
-describe('should work', function () {
-    it('should equal -1', function () {
-        [1,2,3].indexOf(4);
-    });
-});
